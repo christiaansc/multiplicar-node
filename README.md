e89df0a3402f9577197f72d20a4262b3bb80e5d4
Christian <cristiaan.sc@gmail.com>
2020-07-24 14:32:14 -0400

    agregamos readme.md

---
+++ README.md
@@ -0,0 +1,11 @@
+## Multiplicar coonsola APP 
+
+esta es un app para generar archivos de tablas de
+multiplicar
+
+ejecutar este comando
+
+````````````
+npm install
+
+``````````
--- multiplicar/multiplicar.js
+++ multiplicar/multiplicar.js
@@ -39,7 +39,7 @@ let crearArchivo = (base, limite) => {
             if (err)
                 reject(err)
             else
-                resolve(`Archivo tabla-${base} `)
+                resolve(`tabla-${base} `)
         });
 
     });
